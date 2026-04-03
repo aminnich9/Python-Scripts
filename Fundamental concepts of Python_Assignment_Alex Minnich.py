387158af5c6dc2f333e63e8c8245602921404e31
aminnich9 <aminnich9@ivytech.edu>
2026-04-03 11:44:59 -0400

    Add files via upload

---
+++ Fundamental concepts of Python_Assignment_Alex Minnich.py
@@ -0,0 +1,5 @@
+"""This code will Welcome a student to SDEV 120,
+and it will ask for their and a print a message."""
+print("Welcome to SDEV 120 class!")
+name = input("Enter your name:") #This will acquire the user's name
+print("Hello" + name) #prints a greeting to the user with their name
